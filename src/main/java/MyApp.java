7ad1bf9f6707baa2dfe0f7eb1d65e24917731de7
Deepan prabhu Babu <deepan@Deepans-MacBook-Pro-2.local>
2021-06-10 12:29:46 -0700

    type ahead works

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -2,15 +2,21 @@ import com.slack.api.bolt.App;
 import com.slack.api.bolt.jetty.SlackAppServer;
 import com.slack.api.methods.response.views.ViewsOpenResponse;
 import com.slack.api.methods.response.views.ViewsPublishResponse;
+import com.slack.api.app_backend.interactive_components.response.Option;
+import com.slack.api.model.block.composition.PlainTextObject;
 import com.slack.api.model.event.AppHomeOpenedEvent;
 import com.slack.api.model.view.View;
 
 import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import static com.slack.api.model.block.Blocks.*;
 import static com.slack.api.model.block.composition.BlockCompositions.*;
 import static com.slack.api.model.block.element.BlockElements.*;
 import static com.slack.api.model.view.Views.*;
+import static java.util.stream.Collectors.toList;
 
 public class MyApp {
 
@@ -19,31 +25,22 @@ public class MyApp {
                 .callbackId("meeting-arrangement")
                 .type("modal")
                 .notifyOnClose(true)
-                .title(viewTitle(title -> title.type("plain_text").text("Meeting Arrangement").emoji(true)))
+                .title(viewTitle(title -> title.type("plain_text").text("Movie Info").emoji(true)))
                 .submit(viewSubmit(submit -> submit.type("plain_text").text("Submit").emoji(true)))
                 .close(viewClose(close -> close.type("plain_text").text("Cancel").emoji(true)))
                 .privateMetadata("{\"response_url\":\"https://hooks.slack.com/actions/T1ABCD2E12/330361579271/0dAEyLY19ofpLwxqozy3firz\"}")
                 .blocks(asBlocks(
+                        section(section -> section.text(markdownText("Select a Movie"))),
+                        divider(),
                         section(section -> section
                                 .blockId("category-block")
-                                .text(markdownText("Select a category of the meeting!"))
-                                .accessory(staticSelect(staticSelect -> staticSelect
-                                        .actionId("category-selection-action")
-                                        .placeholder(plainText("Select a category"))
-                                        .options(asOptions(
-                                                option(plainText("Customer"), "customer"),
-                                                option(plainText("Partner"), "partner"),
-                                                option(plainText("Internal"), "internal")
-                                        ))
-                                ))
-                        ),
-                        input(input -> input
-                                .blockId("agenda-block")
-                                .element(plainTextInput(pti -> pti.actionId("agenda-action").multiline(true)))
-                                .label(plainText(pt -> pt.text("Detailed Agenda").emoji(true)))
-                        )
-                ))
-        );
+                                .text(plainText(" "))
+                                .accessory(
+                                        multiExternalSelect(conf ->
+                                                conf.actionId("movie-action")
+                                                        .minQueryLength(1)
+                                                        .placeholder(plainText("Enter to search", true))))
+                                ))));
     }
     public static void main(String[] args) throws Exception {
         App app = new App();
@@ -66,10 +63,28 @@ public class MyApp {
             return ctx.ack();
         });
 
+        app.blockSuggestion("movie-action", (req, ctx) -> {
+            final List<Option> allOptions = Arrays.asList(
+                    new Option(plainText("Schedule", true), "schedule"),
+                    new Option(plainText("Budget", true), "budget"),
+                    new Option(plainText("Assignment", true), "assignment")
+            );
+
+            String keyword = req.getPayload().getValue();
+            List<Option> options = allOptions.stream()
+                    .filter(o -> ((PlainTextObject) o.getText()).getText().toLowerCase().contains(keyword))
+                    .collect(toList());
+            return ctx.ack(r -> r.options(options.isEmpty() ? allOptions : options));
+        });
+
         app.blockAction("button-action", (req, ctx) -> {
             ViewsOpenResponse viewsOpenRes = ctx.client().viewsOpen(r -> r
                     .triggerId(ctx.getTriggerId())
                     .view(buildModal()));
+            return ctx.ack();
+        });
+
+        app.endpoint("/queryTmdb", (req, ctx) -> {
             return ctx.ack();
         });
 
