b74c7c1af691cfef248ce60f84d4338dc254872a
Deepan prabhu Babu <deepan@Deepans-MacBook-Pro-2.local>
2021-06-10 16:49:06 -0700

    looks good - except release date

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,33 +1,35 @@
+import com.slack.api.app_backend.interactive_components.response.Option;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.jetty.SlackAppServer;
 import com.slack.api.bolt.util.JsonOps;
 import com.slack.api.methods.response.views.ViewsOpenResponse;
 import com.slack.api.methods.response.views.ViewsPublishResponse;
-import com.slack.api.app_backend.interactive_components.response.Option;
 import com.slack.api.model.block.composition.PlainTextObject;
 import com.slack.api.model.event.AppHomeOpenedEvent;
 import com.slack.api.model.view.View;
+import com.slack.api.model.view.ViewState;
+import info.movito.themoviedbapi.TmdbAccount;
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.model.MovieDb;
 
-import java.time.ZonedDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.slack.api.model.block.Blocks.*;
-import static com.slack.api.model.block.composition.BlockCompositions.*;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
 import static com.slack.api.model.block.element.BlockElements.*;
 import static com.slack.api.model.view.Views.*;
 import static java.util.stream.Collectors.toList;
 
 public class MyApp {
 
-    class PrivateMetadata {
-        String channelId;
-    }
+    static HashMap<Integer, MovieDb> mapOfMovies = new HashMap<>();
 
-    public static View buildModal() {
+    public static View buildMovieSelectionModal() {
         return view(view -> view
-                .callbackId("meeting-arrangement")
+                .callbackId("movie-title-selection")
                 .type("modal")
                 .notifyOnClose(true)
                 .title(viewTitle(title -> title.type("plain_text").text("Movie Info").emoji(true)))
@@ -43,24 +45,72 @@ public class MyApp {
                                 .accessory(
                                         multiExternalSelect(conf ->
                                                 conf.actionId("movie-action")
-                                                        .minQueryLength(1)
-                                                        .placeholder(plainText("Enter to search", true))))
-                                ))));
+                                                .minQueryLength(3)
+                                                .placeholder(
+                                                plainText("Select an Item ( Try Star )", true)
+                                                )))
+                        ))));
     }
+
+    //  Load movies from tmdb
+    public static void loadMovies(){
+        //https://github.com/holgerbrandl/themoviedbapi
+        // Get movies related to star wars
+        // curl --location --request GET 'https://api.themoviedb.org/3/search/movie?api_key=179cfa0a673b02ddfa1a6aa6629fa730&query=star+wars'
+
+        // Hard coded for star wars as in example
+        TmdbMovies movies = new TmdbApi("179cfa0a673b02ddfa1a6aa6629fa730").getMovies();
+        int[] movieIds =   {11,
+                181812,
+                181808,
+                348350,
+                140607,
+                732670,
+                330459,
+                12180,
+                392216,
+                1893,
+                667574,
+                1895,
+                42979,
+                1894,
+                74849,
+                435365,
+                825647,
+                287663,
+                42982,
+                70608};
+
+        int movieIndex = 0;
+        for(int movieId : movieIds){
+            MovieDb movieDb = movies.getMovie(movieId, "en", TmdbMovies.MovieMethod.images, TmdbMovies.MovieMethod.release_dates);
+            mapOfMovies.put(movieId, movieDb);
+        }
+        System.out.println(mapOfMovies.size());
+    }
+
     public static void main(String[] args) throws Exception {
         App app = new App();
+        loadMovies();
 
+        // Display initial home page - and button to act
         app.event(AppHomeOpenedEvent.class, (payload, ctx) -> {
-            // Build a Home tab view
-            ZonedDateTime now = ZonedDateTime.now();
             View appHomeView = view(view -> view
                     .type("home")
                     .blocks(asBlocks(
-                            header(header -> header.text(plainText(pt -> pt.emoji(true).text("*Welcome to Movie Info! :tada:")))),
-                            section(section -> section.text(markdownText("Click the button below to pick a movie !"))),
+                            header(header ->
+                            header.text(plainText(pt -> pt.emoji(true).text("*Welcome to Movie Info! :tada:")))),
+                            section(section ->
+                                    section.text(markdownText("Click the button below to pick a movie !"))),
                             divider(),
-                            actions(actions -> actions.elements(asElements(button(b -> b.actionId("button-action").text(plainText(pt -> pt.text("Select a Movie !"))).value("ping"))))))));
-            // Update the App Home for the given user
+                            actions(actions ->
+                                    actions.elements(
+                                    asElements(
+                                    button(b ->
+                                    b.actionId("home-button-action")
+                                    .text(plainText(pt -> pt.text("Select a Movie !")))
+                                    .value("selected"))))))));
+
             ViewsPublishResponse res = ctx.client().viewsPublish(r -> r
                     .userId(payload.getEvent().getUser())
                     .view(appHomeView)
@@ -68,45 +118,57 @@ public class MyApp {
             return ctx.ack();
         });
 
+        //  Handle button action - show Modal for screen 2
+        app.blockAction("home-button-action", (req, ctx) -> {
+            ViewsOpenResponse viewsOpenRes = ctx.client().viewsOpen(r -> r
+                    .triggerId(ctx.getTriggerId())
+                    .view(buildMovieSelectionModal()));
+            return ctx.ack();
+        });
+
         app.blockSuggestion("movie-action", (req, ctx) -> {
-            final List<Option> allOptions = Arrays.asList(
-                    new Option(plainText("Schedule", true), "schedule"),
-                    new Option(plainText("Budget", true), "budget"),
-                    new Option(plainText("Assignment", true), "assignment")
+            final List<Option> allOptions = new ArrayList<>();
+
+            mapOfMovies.keySet().forEach(movieId ->
+                    allOptions.
+                    add(new Option(plainText(mapOfMovies.get(movieId).getTitle(), true), String.valueOf(movieId)))
             );
 
             String keyword = req.getPayload().getValue();
+
             List<Option> options = allOptions.stream()
                     .filter(o -> ((PlainTextObject) o.getText()).getText().toLowerCase().contains(keyword))
                     .collect(toList());
+
             return ctx.ack(r -> r.options(options.isEmpty() ? allOptions : options));
         });
 
-        app.blockAction("movie-action", (req, ctx) -> {
-            String metaData = req.getPayload().getView().getPrivateMetadata();
-            PrivateMetadata privateMetadata = JsonOps.fromJson(metaData, PrivateMetadata.class);
-            app.client().chatPostMessage(r -> r.channel(privateMetadata.channelId).token(ctx.getBotToken()).text("Thanks !"));
-            return ctx.ack();
-        });
-
-        app.viewSubmission("meeting-arrangement", (req, ctx) -> {
-            app.client().chatPostMessage(r -> r.token(ctx.getBotToken()).channel(ctx.getRequestUserId()).text("hi"));
-            return ctx.ack();
-        });
-
-        app.blockAction("button-action", (req, ctx) -> {
-            ViewsOpenResponse viewsOpenRes = ctx.client().viewsOpen(r -> r
-                    .triggerId(ctx.getTriggerId())
-                    .view(buildModal()));
-            return ctx.ack();
-        });
-
-        app.endpoint("/queryTmdb", (req, ctx) -> {
+        app.viewSubmission("movie-title-selection", (req, ctx) -> {
+            List<Integer> movieIds = fetchMovieId(req.getPayload().getView().getState());
+            for(Integer movieId : movieIds) {
+                MovieDb movieDb = mapOfMovies.get(movieId);
+                app.client().chatPostMessage(r -> r.token(ctx.getBotToken()).channel(ctx.getRequestUserId())
+                        .blocks(asBlocks(
+                                header(t -> t.text(plainText(movieDb.getTitle()))),
+                                divider(),
+                                section(section -> section.text(markdownText("**Release Date:**" + movieDb.getReleaseDate()))),
+                                section(section -> section.text(markdownText(movieDb.getOverview())))
+                        )));
+            }
             return ctx.ack();
         });
 
         SlackAppServer slackAppServer = new SlackAppServer(app);
         slackAppServer.start();
+
+    }
+
+    public static List<Integer> fetchMovieId(ViewState viewState){
+        List<ViewState.SelectedOption> options = viewState.getValues().get("category-block").get("movie-action").getSelectedOptions();
+        return options.stream().map(option -> Integer.parseInt(option.getValue())).collect(Collectors.toList());
+    }
+
+    public static void craftTextMessage(int movieId){
 
     }
 }
