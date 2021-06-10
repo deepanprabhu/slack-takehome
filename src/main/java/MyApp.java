7cf0a22d4ac35e2e1f6b6a1d81522da12646f871
Deepan prabhu Babu <deepan@Deepans-MacBook-Pro-2.local>
2021-06-10 02:23:19 -0700

    modal works

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,6 +1,6 @@
 import com.slack.api.bolt.App;
-import com.slack.api.bolt.AppConfig;
 import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.methods.response.views.ViewsOpenResponse;
 import com.slack.api.methods.response.views.ViewsPublishResponse;
 import com.slack.api.model.event.AppHomeOpenedEvent;
 import com.slack.api.model.view.View;
@@ -8,14 +8,43 @@ import com.slack.api.model.view.View;
 import java.time.ZonedDateTime;
 
 import static com.slack.api.model.block.Blocks.*;
-import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
-import static com.slack.api.model.block.composition.BlockCompositions.plainText;
-import static com.slack.api.model.block.element.BlockElements.asElements;
-import static com.slack.api.model.block.element.BlockElements.button;
+import static com.slack.api.model.block.composition.BlockCompositions.*;
+import static com.slack.api.model.block.element.BlockElements.*;
 import static com.slack.api.model.view.Views.*;
 
 public class MyApp {
 
+    public static View buildModal() {
+        return view(view -> view
+                .callbackId("meeting-arrangement")
+                .type("modal")
+                .notifyOnClose(true)
+                .title(viewTitle(title -> title.type("plain_text").text("Meeting Arrangement").emoji(true)))
+                .submit(viewSubmit(submit -> submit.type("plain_text").text("Submit").emoji(true)))
+                .close(viewClose(close -> close.type("plain_text").text("Cancel").emoji(true)))
+                .privateMetadata("{\"response_url\":\"https://hooks.slack.com/actions/T1ABCD2E12/330361579271/0dAEyLY19ofpLwxqozy3firz\"}")
+                .blocks(asBlocks(
+                        section(section -> section
+                                .blockId("category-block")
+                                .text(markdownText("Select a category of the meeting!"))
+                                .accessory(staticSelect(staticSelect -> staticSelect
+                                        .actionId("category-selection-action")
+                                        .placeholder(plainText("Select a category"))
+                                        .options(asOptions(
+                                                option(plainText("Customer"), "customer"),
+                                                option(plainText("Partner"), "partner"),
+                                                option(plainText("Internal"), "internal")
+                                        ))
+                                ))
+                        ),
+                        input(input -> input
+                                .blockId("agenda-block")
+                                .element(plainTextInput(pti -> pti.actionId("agenda-action").multiline(true)))
+                                .label(plainText(pt -> pt.text("Detailed Agenda").emoji(true)))
+                        )
+                ))
+        );
+    }
     public static void main(String[] args) throws Exception {
         App app = new App();
 
@@ -25,10 +54,10 @@ public class MyApp {
             View appHomeView = view(view -> view
                     .type("home")
                     .blocks(asBlocks(
-                            section(section -> section.text(markdownText(mt -> mt.text(":wave: Hello, App Home! (Last updated: " + now + ")")))),
-                            image(img -> img.imageUrl("https://via.placeholder.com/10/").altText("hi"))
-                    ))
-            );
+                            header(header -> header.text(plainText(pt -> pt.emoji(true).text("*Welcome to Movie Info! :tada:")))),
+                            section(section -> section.text(markdownText("Click the button below to pick a movie !"))),
+                            divider(),
+                            actions(actions -> actions.elements(asElements(button(b -> b.actionId("button-action").text(plainText(pt -> pt.text("Select a Movie !"))).value("ping"))))))));
             // Update the App Home for the given user
             ViewsPublishResponse res = ctx.client().viewsPublish(r -> r
                     .userId(payload.getEvent().getUser())
@@ -36,6 +65,14 @@ public class MyApp {
             );
             return ctx.ack();
         });
+
+        app.blockAction("button-action", (req, ctx) -> {
+            ViewsOpenResponse viewsOpenRes = ctx.client().viewsOpen(r -> r
+                    .triggerId(ctx.getTriggerId())
+                    .view(buildModal()));
+            return ctx.ack();
+        });
+
         SlackAppServer slackAppServer = new SlackAppServer(app);
         slackAppServer.start();
 
